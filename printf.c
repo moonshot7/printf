3831c793cb302d00ec4944b19e1012fb8f8f28f9
zahiiir <simo222zahir@gmail.com>
2023-11-17 15:04:18 -0800

    update

--- printf.c
+++ printf.c
@@ -1,4 +1,8 @@
 #include "main.h"
+#include <stdarg.h>
+#include "printf.h"
+#include "sepcifier.c"
+
 
 
 /**
@@ -9,37 +13,38 @@
 
 int printf(const char *format, ...)
 {
-int sum = 0;
-va_list ap;
-char *p, *start;
-params_t params = PARAMS_INIT;
+    int sum = 0;
+    va_list ap;
+    char *p, *start;
+    params_t params = PARAMS_INIT;
 
-va_start(ap, format);
-if (!format || (format[0] == '%' && !format[1]))
-	return (-1);
-if (format[0] == '%' && format[1] == ' ' && !format[2])
-	return (-1);
+    va_start(ap, format);
+    if (format == NULL || (format[0] == '%' && !format[1]))
+        return (-1);
+    if (format[0] == '%' && format[1] == ' ' && !format[2])
+        return (-1);
+
     for (p = (char *)format; *p; p++)
     {
-       init_params(&params, ap);
-       if (*p != '%')
-       {
-           sum += _putchar(*p);
-           continue;
-       }
-       start = p;
-       p++;
-       while (get_flag(p, &params))
-       {
-           p++;
-       }
-       p = get_width(p, &params, ap);
-       p = get_precision(p, &params, ap);
-       if (get_modifier(p, &params))
-           p++;
-       if (!get_specifier(p))
-           sum += print_from_to(start, p, params.l_modifier || params.h_modifier ? p - 1 : 0);
-       else
+        init_params(&params, ap);
+        if (*p != '%')
+        {
+            sum += _putchar(*p);
+            continue;
+        }
+        start = p;
+        p++;
+        while (get_flag(p, &params))
+        {
+            p++;
+        }
+        p = get_width(p, &params, ap);
+        p = get_precision(p, &params, ap);
+        if (get_modifier(p, &params))
+            p++;
+        if (!get_specifier(p))
+            sum += print_from_to(start, p, params.l_modifier || params.h_modifier ? p - 1 : 0);
+        else
             sum += get_print_func(p, ap, &params);
     }
     _putchar(BUF_FLUSH);
